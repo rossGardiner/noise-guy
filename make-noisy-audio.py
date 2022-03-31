d72d2afee2159551406b7be94ba55a458bcd4e98
= <rossgardiner24@gmail.com>
2022-03-31 19:57:54 +0100

    fixes bugs

--- make-noisy-audio.py
+++ make-noisy-audio.py
@@ -5,10 +5,6 @@ import librosa
 import numpy as np
 from scipy.io import wavfile
 import random
-import struct
-
-
-SAMPLE_RATE = 22050
 
 def dir_path(string):
     if os.path.isdir(string):
@@ -32,6 +28,7 @@ parser.add_argument('--nr_clean', type=int, help='number of clips to use from ea
 parser.add_argument('--nr_noise', type=int, help='number of noise clips to use', default=10)
 parser.add_argument('--rand', type=bool, help='take a random audio file from each genre rather than the first', default=False)
 parser.add_argument('--upsample', type=bool, help='upsample the used gtzan files to 44khz', default=True)
+parser.add_argument('--noise_gain', type=float, help='gain of noise in generated data, as a fraction of the clean data gain', default=0.7)
 args = parser.parse_args()
 print(args)
 
@@ -50,11 +47,13 @@ if args.upsample:
         os.makedirs(args.out + '/upsampled')
     for clean in clean_files:
         clean_name = os.path.splitext(os.path.basename(clean))[0]
-        clean_data, s = librosa.load(clean, sr=SAMPLE_RATE)
-        resampled = librosa.resample(y=clean_data, orig_sr=SAMPLE_RATE, target_sr=SAMPLE_RATE * 2)
+        clean_data, s = librosa.load(clean)
+        print("sample rate of pre-upsampled clean data, {}, {}Hz".format(clean, s))
+        resampled = librosa.resample(y=clean_data, orig_sr=s, target_sr=44100)
+        resampled *= (1.0 - args.noise_gain)
         filename = args.out + '/upsampled/{}.wav'.format(clean_name)
         upsampled_files.append(filename)
-        wavfile.write(filename, SAMPLE_RATE * 2, resampled)
+        wavfile.write(filename, 44100, resampled)
 
 noise_files = []
 for dir in glob(args.noise + '/*')[:args.nr_noise]:
@@ -62,34 +61,41 @@ for dir in glob(args.noise + '/*')[:args.nr_noise]:
     noise_files.append(file)
 
 output = []
-count = 0
 for noise in noise_files:
     noise_name = os.path.splitext(os.path.basename(noise))[0]
-    noise_data, s = librosa.load(noise, sr=SAMPLE_RATE)
+    s, noise_data = wavfile.read(noise)
+    noise_data = noise_data.astype(np.float32) / (2**16)
+    print("name: {} shape: {}".format(noise, noise_data.shape))
+    if len(noise_data.shape) >= 2:
+        print('invalid noise shape')
+        noise_data = np.ravel(noise_data)
+        noise_data = np.delete(noise_data, np.arange(0, noise_data.size, 2))
     middle = len(noise_data) // 2
+    print("for {}, sample rate :{}, length: {}".format(noise, s, len(noise_data)))
+
     files = upsampled_files if args.upsample else clean_files
-    sr = SAMPLE_RATE * 2 if args.upsample else SAMPLE_RATE
     for clean in files:
         clean_name = os.path.splitext(os.path.basename(clean))[0]
-        clean_data, s = librosa.load(clean, sr=sr)
+        s, clean_data = wavfile.read(clean)
+        print(clean_data.shape)
+        clean_data = clean_data.astype(np.float32)
+
+        print("for {}, sample rate :{}, length: {}".format(clean, s, len(clean_data)))
 
         l = len(clean_data)
+        print(l)
         noise_slice = noise_data
         diff = l - len(noise_slice)
         if diff > 0:
             noise_slice = np.pad(noise_data, pad_width=diff//2, mode='edge')
         else:
             noise_slice = noise_slice[middle - l//2: middle + l//2]
-
+        noise_slice *= args.noise_gain
         new_data = clean_data + noise_slice
         if not os.path.exists(args.out + '/noisy'):
             os.makedirs(args.out + '/noisy')
-        wavfile.write(args.out + '/noisy/{}-{}.wav'.format(clean_name, noise_name), s, new_data)
+        wavfile.write(args.out + '/noisy/{}-{}.wav'.format(clean_name, noise_name), s, new_data.astype(np.float32))
 
-
-        count += 1
-    #    print(rate)
-    #print(len(data_noise))
 
 
 
